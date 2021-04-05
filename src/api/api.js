e59f6bb9298bfeaab2345c71902066f964f81e17
10dimensions <relay.10d@gmail.com>
2021-04-06 00:22:45 +0530

    Install and Test Ceramic

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,20 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { gql } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPH_URI,
+import CeramicClient from '@ceramicnetwork/http-client'
+
+
+const GRAPH_URL = process.env.REACT_APP_GRAPH_URI;
+const CERAMIC_URL = process.env.REACT_APP_CERAMIC_URI;
+
+
+const graphClient = new ApolloClient({
+  uri: GRAPH_URL,
   cache: new InMemoryCache()
 });
+
+const ceramic = new CeramicClient(CERAMIC_URL)
+
 
 const LOCATION_LOOKUP_QUERY = gql`
     query {
@@ -17,25 +27,35 @@ const LOCATION_LOOKUP_QUERY = gql`
                 rootCID
                 }
             }
-            }
+          }
         }`
 
-const locationBasedLookup = async () => {
 
-    let result = await client.query({
+const geoLookup = async () => {
+
+    let result = await graphClient.query({
         query: LOCATION_LOOKUP_QUERY
     })
    
     let rootCID = ""
 
-      try {
-        rootCID = result['data']['geoWebCoordinate']['landParcel']['license']['rootCID'];
-      }
-      catch(e){
-          console.error(e);
-      }
-      
-      return rootCID;
+    try {
+      rootCID = result['data']['geoWebCoordinate']['landParcel']['license']['rootCID'];
+    }
+    catch(e){
+        console.error(e);
+    }
+    
+    return rootCID;
 }
 
-export {locationBasedLookup};
+
+const parcelLookup = async(docid) => {
+
+  const doc = await ceramic.loadDocument(docid)
+  //console.log(doc); 
+  return doc;
+
+}
+
+export {geoLookup, parcelLookup};
--- src/components/gps/GPS.jsx
+++ src/components/gps/GPS.jsx
@@ -1,20 +1,20 @@
 import React, {useState, useEffect} from 'react';
-
-import {locationBasedLookup} from '../../api/api';
+import {geoLookup, parcelLookup} from '../../api/api';
 
 const GeoWebCoordinate = require("js-geo-web-coordinate");
 
+
 const GPS = () => {
 
-    const initCoordinate = {lat: 0, lon: 0}
-    const [coordinate, setCoordinate] = useState(initCoordinate);
-    const [gwCoord, setGwCoord] = useState("");
-    const [rootCid, setRootCid] = useState("");
+    const initCoordinate = {lat: 0, lon: 0}; //default lat, lon
+    const [coordinate, setCoordinate] = useState(initCoordinate);   //gps coordinates {lat, lon}
+    const [gwCoord, setGwCoord] = useState(""); //geowebcoordinates as string
+    const [rootCid, setRootCid] = useState(""); //rootCid
 
-
+    //On Mount
     useEffect( ()=>{
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
+            navigator.geolocation.getCurrentPosition(showPosition); //Get Position, with callback
         } else {
             console.log("Geolocation is not supported by this browser.");
             setCoordinate({lat: -1, lon: -1});
@@ -23,18 +23,27 @@ const GPS = () => {
 
 
     const showPosition = (position) => {
-        const {latitude, longitude} = position.coords;
-        setCoordinate({lat: latitude, lon: longitude});
 
-        const _gwCoord = GeoWebCoordinate.from_gps(longitude, latitude);
+        const {latitude, longitude} = position.coords;
+        setCoordinate({lat: latitude, lon: longitude}); //Set Lat and Lon state
+
+        const _gwCoord = GeoWebCoordinate.from_gps(longitude, latitude);    //Convert Lon, Lat to GeoWebCoordinate
         setGwCoord(_gwCoord.toString());
         
         getRoootCid();
     }   
 
     const getRoootCid = async () => {
-        const _rootCid = await locationBasedLookup();
+        const _rootCid = await geoLookup();
         setRootCid(_rootCid);
+
+        getParcel('kjzl6cwe1jw147y87g1r5kxffmrzvdw9bihqergw7um43frrd9ykqet6dsu3s8s');
+    }
+
+    const getParcel = async(_docid) => {
+
+        const _parcelData = await parcelLookup(_docid);
+        console.log(_parcelData);
     }
 
 

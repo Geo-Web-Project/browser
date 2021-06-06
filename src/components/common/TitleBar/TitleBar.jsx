3123204d56c9cdc9d79ad04a4bed7e6087a90dea
10dimensions <relay.10d@gmail.com>
2021-06-06 14:11:31 +0530

    Setup relocalization

--- src/components/common/TitleBar/TitleBar.jsx
+++ src/components/common/TitleBar/TitleBar.jsx
@@ -2,7 +2,9 @@ import React, {useState, useEffect} from 'react';
 import './styles.css';
 
 //Page Layout including Title and Background 
-const TitleBar = () => {
+const TitleBar = (props) => {
+
+    const accessGps = props.accessGps;
 
     return(
         <div className="layout-div">
@@ -10,6 +12,7 @@ const TitleBar = () => {
                 <div className="logo" />
                 <div className="title-txt">{'Geo Web'}</div>
                 <div className="title-caption">{'Browse Earth'}</div>
+                <div className="gps-location" onClick={()=>accessGps()}></div>
             </div>
         </div>
     );
--- src/container/GeoWebSystem/GWS.jsx
+++ src/container/GeoWebSystem/GWS.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {getGeoId, getParcelInfo, getParcelContent} from '../../api/api';
 
-import Layout from '../../components/common/TitleBar/TitleBar';
+import TitleBar from '../../components/common/TitleBar/TitleBar';
 import GWLoader from '../../components/common/Loader/Loader';
 import GWAvail from '../../components/common/ContentFiller/Avail';
 import GWInfo from '../GeoWebInterface/components/GeoWebInfo/GWInfo';
@@ -27,20 +27,29 @@ const GWS = () => {
 
     //On Mount
     useEffect( ()=>{
+        accessGps();
+    }, [] );
+
+    const accessGps = () => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition); //Get Position, with callback
-        } else {
+        } 
+        else {
             console.log("Geolocation is not supported by this browser.");
             setCoordinate({lat: -1, lon: -1});
         }
-    }, [] );
+    }
 
 
     const showPosition = (position) => {
 
+        SetLoading(true);
+
         //hard-coded coordinates for testing
         const latitude = -69.750; 
         const longitude = 71.000;
+
+        debugger;
 
         //const {latitude, longitude} = position.coords;
         setCoordinate({lat: latitude, lon: longitude}); //Set Lat and Lon state
@@ -99,7 +108,7 @@ const GWS = () => {
             // Returns Info Expandable and Parcel Content
             return (
                 <div className="layout-root">
-                    <GWInfo gwInfo={gwInfo} gwContentName={gwContent?gwContent.name:""}/>
+                    <GWInfo gwInfo={gwInfo} gwContentName={gwContent?gwContent.name:""} showPosition={showPosition} />
                     <GWContent gwWebContent={gwContent?gwContent.webContent:null}
                         gwCanvasContent={gwContent?gwContent.mediaContent:null}/>
                 </div>
@@ -108,7 +117,7 @@ const GWS = () => {
         else{
             return (
                 <div className="layout-root">
-                    <GWInfo gwInfo={null} gwContentName={"No Parcel Found"}/>
+                    <GWInfo gwInfo={null} gwContentName={"No Parcel Found"} showPosition={showPosition} />
                     <GWAvail />
                 </div>
             );
@@ -118,7 +127,7 @@ const GWS = () => {
 
     return(
         <div>
-            <Layout />
+            <TitleBar accessGps={accessGps} />
 
             { loading ? <GWLoader/> : <GeoWeb /> }
 

9584ba986ec19b243cad5fd148ab087278034b4f
tnrdd <tnrdd@tutanota.com>
2022-10-10 12:29:38 +0200

    fix: space filler content

--- src/components/common/ContentFiller/Empty.jsx
+++ src/components/common/ContentFiller/Empty.jsx
@@ -8,7 +8,7 @@ const GWEmpty = (props) => {
   const prompt = `Enjoy the beauty of empty space...The current land parcel holder has not added ${promptType} content here yet.`;
 
   return (
-    <div style={{ position: "absolute" }}>
+    <div className={styles["wrapper"]}>
       <div className={styles["rect-box"]} />
       <span className={styles["empty-txt"]}>{prompt}</span>
     </div>

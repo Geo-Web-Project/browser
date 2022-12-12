18fb21cf2e241c7975e91b43036a863c1bb3eaed
Cody Hatfield <cody.hatfield@me.com>
2022-12-12 14:17:41 -0800

    Consolidate to one .env

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,15 +1,10 @@
-export const NETWORK_ID = 5;
-export const NETWORK_NAME = "Goerli";
+export const NETWORK_ID = parseInt(process.env.NEXT_PUBLIC_NETWORK_ID);
 export const PAYMENT_TOKEN = "ETHx";
 export const SUBGRAPH_URL = process.env.NEXT_PUBLIC_GRAPH_URI;
 
 export const CERAMIC_URL = process.env.NEXT_PUBLIC_CERAMIC_URI;
-export const CONNECT_NETWORK = "testnet-clay";
+export const CONNECT_NETWORK = process.env.NEXT_PUBLIC_CERAMIC_CONNECT_NETWORK;
 export const CERAMIC_EXPLORER = `https://cerscan.com/${CONNECT_NETWORK}/stream`;
-
-export const IPFS_BOOTSTRAP_PEER =
-  "/dns4/preload.ipfs.geoweb.network/tcp/4002/wss/p2p/12D3KooWKcc7Jz6jxCJeq8LRi6chufG16hbezYnT2DUhSZRy3whU";
-export const IPFS_PRELOAD_NODE = "/dns4/preload.ipfs.geoweb.network/https";
 
 export const PINATA_API_ENDPOINT = "https://api.pinata.cloud/psa";
 export const STORAGE_WORKER_ENDPOINT = "https://storage-workers.geo-web.workers.dev";

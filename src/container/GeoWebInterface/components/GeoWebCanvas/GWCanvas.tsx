ec2eb83a6035ced5b11b0429e1019fa0d407d054
tnrdd <tnrdd@tutanota.com>
2022-12-28 17:51:00 +0100

    fix: support USDZ on compatible devices (#61)
    * fix: support USDZ on compatible devices
    
    * refactor: remove redundant dep

--- src/container/GeoWebInterface/components/GeoWebCanvas/GWCanvas.tsx
+++ src/container/GeoWebInterface/components/GeoWebCanvas/GWCanvas.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState, useRef } from "react";
-import GWEmpty from "../../../../components/common/ContentFiller/Empty";
-import ContentLabel from "../../../../components/common/ContentLabel/ContentLabel";
 import { GeoWebContent } from "@geo-web/content";
 import { MediaGallery, MediaObject } from "@geo-web/types";
+import GWEmpty from "../../../../components/common/ContentFiller/Empty";
+import ContentLabel from "../../../../components/common/ContentLabel/ContentLabel";
 import styles from "./styles.module.css";
 
 const gwGateway = process.env.NEXT_PUBLIC_MODEL_VIEWER_IPFS_GATEWAY;
@@ -13,15 +13,24 @@ export type GWCanvasProps = {
 };
 
 const ModelViewer = (props: any) => {
-  let url = props.url;
-  let modelRef = props.modelRef;
+  const { url, modelRef, isUsdzModel } = props;
+
+  useEffect(() => {
+    if (url && !isUsdzModel) {
+      modelRef.current.dismissPoster();
+    } else {
+      modelRef.current.showPoster();
+    }
+  }, [url, isUsdzModel]);
 
   return (
-    // @ts-ignore
+    /* @ts-ignore */
     <model-viewer
       ref={modelRef}
       className={styles["gwCanvas"]}
       src={url}
+      ios-src={isUsdzModel ? url : ""}
+      reveal="manual"
       environment-image="neutral"
       shadow-intensity="1"
       ar
@@ -35,6 +44,18 @@ const ModelViewer = (props: any) => {
         <img alt="AR prompt" id="ar-prompt-img" />
       </div>
       <button id="ar-failure">AR is not tracking!</button>
+      {isUsdzModel ? (
+        <div className={styles["ar-only"]} slot="poster">
+          This model is only viewable on iPhone and iPad AR
+        </div>
+      ) : (
+        <img
+          className={styles["model-loading"]}
+          src="/assets/spinner.svg"
+          alt="loading"
+          slot="poster"
+        />
+      )}
       {/* @ts-ignore */}
     </model-viewer>
   );
@@ -42,11 +63,13 @@ const ModelViewer = (props: any) => {
 
 const GWCanvas = (props: GWCanvasProps) => {
   const { mediaGallery, gwContent } = props;
-  const [modelIndex, setModelIndex] = useState(0);
-  const modelRef = useRef();
 
+  const [modelIndex, setModelIndex] = useState(0);
   const [modelUrl, setModelUrl] = useState<string | undefined>(undefined);
   const [modelName, setModelName] = useState<string | undefined>(undefined);
+  const [isUsdzModel, setIsUsdzModel] = useState<boolean>(false);
+
+  const modelRef = useRef();
 
   useEffect(() => {
     const loadObj = async () => {
@@ -60,7 +83,12 @@ const GWCanvas = (props: GWCanvasProps) => {
         "/",
         {}
       );
-      setModelUrl(`${gwGateway}/ipfs/` + mediaObject.content.toString());
+      const _isUsdzModel = mediaObject.encodingFormat === "model/vnd.usdz+zip";
+      const fileName = _isUsdzModel ? `?filename=${mediaObject.name}.usdz` : "";
+      const contentUrl = `${gwGateway}/ipfs/${mediaObject.content.toString()}/${fileName}`;
+
+      setIsUsdzModel(_isUsdzModel);
+      setModelUrl(contentUrl);
       setModelName(mediaObject.name);
     };
 
@@ -99,13 +127,11 @@ const GWCanvas = (props: GWCanvasProps) => {
         {mediaGallery.length > 1 && (
           <button className={styles["clk-left"]} onClick={() => clickLeft()} />
         )}
-        {modelUrl ? (
-          <ModelViewer modelRef={modelRef} url={modelUrl} />
-        ) : (
-          <div className={styles["model-loading"]}>
-            <img src="/assets/spinner.svg" alt="loading" />
-          </div>
-        )}
+        <ModelViewer
+          modelRef={modelRef}
+          url={modelUrl}
+          isUsdzModel={isUsdzModel}
+        />
         {mediaGallery.length > 1 && (
           <button
             className={styles["clk-right"]}

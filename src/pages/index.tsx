b8565203d68dd256ab6a45f5bc60897fc973e114
tnrdd <tnrdd@tutanota.com>
2023-02-10 00:54:17 +0100

    feat: add landing modal for first time users (#81)
    * feat: add landing modal for first time users
    
    * feat: change link to docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,22 @@ import { getIpfs, providers } from "ipfs-provider";
 import * as IPFSCore from "ipfs-core";
 import * as IPFSHttpClient from "ipfs-http-client";
 import { GeoWebContent } from "@geo-web/content";
+import GWLoader from "../components/common/Loader/Loader";
 import GWS from "../container/GeoWebSystem/GWS";
+import LandingModal from "../components/common/LandingModal/LandingModal";
 
 const { jsIpfs, httpClient } = providers;
 
 export default function Index() {
   const [gwContent, setGWContent] = useState<GeoWebContent | null>(null);
+  const [hasAgreedModal, setHasAgreedModal] = useState<boolean | null>(null);
 
   useEffect(() => {
     (async () => {
+      setHasAgreedModal(
+        localStorage.getItem("gwHasAgreedModal") ? true : false
+      );
+
       const ceramic = new CeramicClient(CERAMIC_URL);
 
       const { ipfs, provider, apiAddress } = await getIpfs({
@@ -53,5 +60,11 @@ export default function Index() {
     })();
   }, []);
 
-  return <GWS gwContent={gwContent} />;
+  if (hasAgreedModal === null) {
+    return <GWLoader />;
+  } else if (hasAgreedModal) {
+    return <GWS gwContent={gwContent} />;
+  } else {
+    return <LandingModal setHasAgreedModal={setHasAgreedModal} />;
+  }
 }

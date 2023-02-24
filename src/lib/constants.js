5b879d51e9ec48ff92791835bb15ee4d73d1fc38
Cody Hatfield <cody.hatfield@me.com>
2023-02-24 14:39:21 -0800

    Use Geo Web run delegate node

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -5,6 +5,7 @@ export const SUBGRAPH_URL = process.env.NEXT_PUBLIC_GRAPH_URI;
 export const CERAMIC_URL = process.env.NEXT_PUBLIC_CERAMIC_URI;
 export const CONNECT_NETWORK = process.env.NEXT_PUBLIC_CERAMIC_CONNECT_NETWORK;
 export const CERAMIC_EXPLORER = `https://cerscan.com/${CONNECT_NETWORK}/stream`;
+export const IPFS_DELEGATE = process.env.NEXT_PUBLIC_IPFS_DELEGATE;
 
 export const PINATA_API_ENDPOINT = "https://api.pinata.cloud/psa";
 export const STORAGE_WORKER_ENDPOINT = "https://storage-workers.geo-web.workers.dev";

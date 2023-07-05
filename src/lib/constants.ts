b99af84af317bfb33ccbf8e6e94928d00e453419
tnrdd <tnrdd@tutanota.com>
2023-07-05 10:40:59 +0200

    feat: add support for content retrieval with content hash

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,11 @@
-export const NETWORK_ID = parseInt(process.env.NEXT_PUBLIC_NETWORK_ID);
+export const NETWORK_ID = parseInt(process.env.NEXT_PUBLIC_NETWORK_ID!);
 export const PAYMENT_TOKEN = "ETHx";
 export const SUBGRAPH_URL = process.env.NEXT_PUBLIC_GRAPH_URI;
-
+export const GRAPH_URL = process.env.NEXT_PUBLIC_GRAPH_URI;
 export const CERAMIC_URL = process.env.NEXT_PUBLIC_CERAMIC_URI;
 export const CONNECT_NETWORK = process.env.NEXT_PUBLIC_CERAMIC_CONNECT_NETWORK;
 export const CERAMIC_EXPLORER = `https://cerscan.com/${CONNECT_NETWORK}/stream`;
 export const IPFS_DELEGATE = process.env.NEXT_PUBLIC_IPFS_DELEGATE;
-
 export const PINATA_API_ENDPOINT = "https://api.pinata.cloud/psa";
 export const STORAGE_WORKER_ENDPOINT = "https://storage-workers.geo-web.workers.dev";
-
 export const SECONDS_IN_YEAR = 60 * 60 * 24 * 365;

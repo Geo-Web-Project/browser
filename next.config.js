b6efaea0a33b1889be5a460da3b262606160a86f
Cody Hatfield <cody.hatfield@me.com>
2023-09-13 18:58:45 +0200

    Enable static export

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { parsed: myEnv } = require('dotenv').config({ path: `.env.${process.env.A
 if(!myEnv) console.log("Environment 'APP_ENV' is not set")
 
 module.exports = {
+  output: 'export',
   webpack(config) {
       config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
       return config

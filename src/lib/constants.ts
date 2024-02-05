d0f8dc0c5a86c61699570445e6bd948189fc43a3
Cody Hatfield <cody.hatfield@me.com>
2024-02-04 19:13:59 -0800

    feat: OP Sepolia (#98)
    * feat: move to op-sepolia
    
    * build: update sdk version

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -12,6 +12,17 @@ export const PINATA_API_ENDPOINT = "https://api.pinata.cloud/psa";
 export const STORAGE_WORKER_ENDPOINT =
   "https://storage-workers.geo-web.workers.dev";
 export const SECONDS_IN_YEAR = 60 * 60 * 24 * 365;
-export const WS_RPC_URL = import.meta.env.VITE_WS_RPC_URL!;
-export const HTTP_RPC_URL = import.meta.env.VITE_HTTP_RPC_URL!;
 export const LIVEPEER_API_KEY = import.meta.env.VITE_LIVEPEER_API_KEY!;
+
+export const RPC_URLS_HTTP: Record<number, string> = {
+  10: `https://opt-mainnet.g.alchemy.com/v2/${import.meta.env
+    .VITE_ALCHEMY_MAINNET_API_KEY!}`,
+  11155420: `https://opt-sepolia.g.alchemy.com/v2/${import.meta.env
+    .VITE_ALCHEMY_TESTNET_API_KEY!}`,
+};
+export const RPC_URLS_WS: Record<number, string> = {
+  10: `wss://opt-goerli.g.alchemy.com/v2/${import.meta.env
+    .VITE_ALCHEMY_MAINNET_API_KEY!}`,
+  11155420: `wss://opt-sepolia.g.alchemy.com/v2/${import.meta.env
+    .VITE_ALCHEMY_TESTNET_API_KEY!}`,
+};
